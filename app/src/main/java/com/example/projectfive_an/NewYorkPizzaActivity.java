e410f642ad73e39ea8ba053e50a0b40cecbec7cd
anicaaovo <93107633+anicaaovo@users.noreply.github.com>
2022-12-07 21:52:52 -0500

    hurry

--- app/src/main/java/com/example/projectfive_an/NewYorkPizzaActivity.java
+++ app/src/main/java/com/example/projectfive_an/NewYorkPizzaActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -47,36 +48,43 @@ public class NewYorkPizzaActivity extends AppCompatActivity {
     /**
      * Default Constructor
      */
-    public NewYorkPizzaActivity(){
+    public NewYorkPizzaActivity() {
 
     }
-    private void initializeRecyclerViewListener(){
+
+    private void initializeRecyclerViewListener() {
         listener = new FlavorAdp.onClickListener() {
             @Override
             public void clicked(String flavor) {
-                if (flavor.equals("BBQ Chicken")){
+                if (flavor.equals("BBQ Chicken")) {
                     curPizza = pizzaFactory.createBBQChicken();
                     newYorkTitle.setText("New York Style Pizza - Current Selecting: " + "BBQ " +
                             "Chicken flavor");
-                }else if (flavor.equals("Deluxe")){
+                } else if (flavor.equals("Deluxe")) {
                     curPizza = pizzaFactory.createDeluxe();
                     newYorkTitle.setText("New York Style Pizza - Current Selecting: Deluxe flavor");
-                }else if(flavor.equals("Meatzza")){
+                } else if (flavor.equals("Meatzza")) {
                     curPizza = pizzaFactory.createMeatzza();
                     newYorkTitle.setText("New York Style Pizza - Current Selecting: Meatzza " +
                             "flavor");
-                }else if(flavor.equals("BuildYourOwn")){
+                } else if (flavor.equals("BuildYourOwn")) {
                     curPizza = pizzaFactory.createBuildYourOwn();
                     newYorkTitle.setText("New York Style Pizza - Current Selecting: BuildTourOwn " +
                             "flavor");
                 }
+                crustDescription.setText("Crust: " + curPizza.getCrust().getCrustText());
+                if (size != null){
+                    curPizza.setSize(size);
+                }
+                updatePrice(curPizza.price());
             }
         };
     }
+
     /**
      * This method initialize UI components for New York Style Pizza ordering page.
      */
-    private void initializeUI(){
+    private void initializeUI() {
         backButton = findViewById(R.id.backButton);
         newYorkTitle = findViewById(R.id.newYorkTitle);
         pizzaChoosing = findViewById(R.id.pizzaChoosing);
@@ -91,11 +99,11 @@ public class NewYorkPizzaActivity extends AppCompatActivity {
         realToppings = findViewById(R.id.realToppings);
         crustDescription = findViewById(R.id.crustDescription);
         price = findViewById(R.id.price);
-        imageList = Arrays.asList(R.drawable.ny_deluxe,R.drawable.ny_bbq,R.drawable.ny_meatzza,
+        imageList = Arrays.asList(R.drawable.ny_deluxe, R.drawable.ny_bbq, R.drawable.ny_meatzza,
                 R.drawable.createyourown);
-        flavorList = Arrays.asList("Deluxe","BBQ Chicken","Meatzza","BuildYourOwn");
+        flavorList = Arrays.asList("Deluxe", "BBQ Chicken", "Meatzza", "BuildYourOwn");
         initializeRecyclerViewListener();
-        flavorAdp = new FlavorAdp(this,imageList,flavorList,listener);
+        flavorAdp = new FlavorAdp(this, imageList, flavorList, listener);
         pizzaChoosing.setLayoutManager(new LinearLayoutManager(this));
         pizzaChoosing.setItemAnimator(new DefaultItemAnimator());
         pizzaChoosing.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.
@@ -107,10 +115,10 @@ public class NewYorkPizzaActivity extends AppCompatActivity {
      * This method is to set a listener to the back button. Once it is clicked, it will finish the
      * current activity and go back to the main page.
      */
-    private void clickBackButton(){
-        backButton.setOnClickListener(new View.OnClickListener(){
+    private void clickBackButton() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 newYorkTitle.setText("New York Style Pizza");
                 finish();
             }
@@ -120,71 +128,90 @@ public class NewYorkPizzaActivity extends AppCompatActivity {
     /**
      * This method provides services for receiving which size of pizza is chosen.
      */
-    private void sizeSelection(){
-        /*if (sizeAlert()){
-            smallButton.setClickable(false);
-            mediumButton.setClickable(false);
-            largeButton.setClickable(false);
-            return;
-        }else{
-            smallButton.setClickable(true);
-            mediumButton.setClickable(true);
-            largeButton.setClickable(true);
-        }*/
-        smallButton.setOnClickListener(new View.OnClickListener(){
+    private void sizeSelection() {
+        smallButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 size = Size.SMALL;
+                if (curPizza != null && size != null) {
+                    curPizza.setSize(size);
+                    updatePrice(curPizza.price());
+                }
             }
         });
-        mediumButton.setOnClickListener(new View.OnClickListener(){
+        mediumButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 size = Size.MEDIUM;
+                if (curPizza != null && size != null) {
+                    curPizza.setSize(size);
+                    updatePrice(curPizza.price());
+                }
             }
         });
-        largeButton.setOnClickListener(new View.OnClickListener(){
+        largeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 size = Size.LARGE;
+                if (curPizza != null && size != null) {
+                    curPizza.setSize(size);
+                    updatePrice(curPizza.price());
+                }
             }
         });
     }
 
-   /* private boolean sizeAlert(){
-        if (curPizza == null){
+    private void generateAlert(int state) {
+        if(state == 1) {
             AlertDialog.Builder alert = new AlertDialog.Builder(this);
-            alert.setTitle("Please select the pizza flavor on the left first");
-            alert.setMessage("You are not allowed to select size before selecting flavor");
+            alert.setTitle("Stop!");
+            alert.setMessage("Build Your Own pizza could only have 7 toppings! ");
             AlertDialog dialog = alert.create();
             dialog.show();
-            return true;
         }
-        return false;
-    }*/
+    }
 
-    private void toppingSelection(){
+    /**
+     * This method is used for initializing the topping list and add listener to the listview.
+     */
+    private void toppingSelection() {
         Topping[] toppings = Topping.values();
         String[] stringedToppings = new String[toppings.length];
         for (int i = 0; i < toppings.length; i++) {
             stringedToppings[i] = toppings[i].getToppingText();
         }
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice,
                 stringedToppings);
         realToppings.setChoiceMode(realToppings.CHOICE_MODE_MULTIPLE);
         realToppings.setAdapter(adapter);
-        realToppings.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        realToppings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
+                if (curPizza instanceof BuildYourOwn) {
+                    if (curPizza.add(toppings[i])) {
+                        realToppings.setItemChecked(i, true);
+                        updatePrice(curPizza.price());
+                    } else if (curPizza.remove(toppings[i])) {
+                        realToppings.setItemChecked(i, false);
+                        updatePrice(curPizza.price());
+                    } else if (curPizza.getSelectedToppings().size() == MAXTOPPINGS) {
+                        generateAlert(1);
+                    }
+                }
             }
         });
     }
 
-    private void priceCheck(){
-
+    /**
+     * This method is used for updating the price after new selections are made.
+     */
+    private void updatePrice(double newPrice) {
+        price.setText("Pizza Price $: " + newPrice);
     }
 
+    private void clickAddToCart(){
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -193,6 +220,5 @@ public class NewYorkPizzaActivity extends AppCompatActivity {
         clickBackButton();
         sizeSelection();
         toppingSelection();
-        priceCheck();
     }
 }
